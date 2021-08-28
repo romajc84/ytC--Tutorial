212a2d2625a6755399307c2343249c0435f05c7f
romajc84 <joeyroman84@gmail.com>
2021-08-28 14:30:09 -0400

    13 - Using Variables in Classes

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class BuckysClass
-{
-public:
-    void coolSaying()
-    {
-        cout << "preachin to the choir" << endl;
+class BuckysClass {
+   public:
+    void setName(string x) {
+        name = x;
     }
+    string getName() {
+        return name;
+    }
+
+   private:
+    string name;
 };
 
-int main()
-{
-    BuckysClass buckysObject;
-    buckysObject.coolSaying();
+int main() {
+    BuckysClass bo;
+    bo.setName("Sir Bucky Wallace");
+    cout << bo.getName();
     return 0;
 }
