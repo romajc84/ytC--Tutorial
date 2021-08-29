2ad7bb31415d9576a9cf15bbb5ff71d7b5100d53
romajc84 <joeyroman84@gmail.com>
2021-08-29 09:51:40 -0400

    19 - Simple Program Using a Loop

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
-#include "Burrito.cpp"
-#include "Burrito.h"
-
 using namespace std;
 
 int main() {
-    Burrito bo;
+    int x = 1;
+    int number;
+    int total = 0;
+
+    while (x <= 5) {
+        cin >> number;
+        total = total + number;
+        x++;
+    }
+
+    cout << "Your total is " << total << endl;
+
     return 0;
 }
