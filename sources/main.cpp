5fc19fd654c2933814a42c0ff7466e11345d9c2b
romajc84 <joeyroman84@gmail.com>
2021-08-30 11:05:18 -0400

    25 - Switch

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,10 +4,19 @@
 using namespace std;
 
 int main() {
-    int x = 99;
+    int age = 18;
 
-    do {
-        cout << x << endl;
-        x++;
-    } while (x < 10);
+    switch (age) {
+        case 16:
+            cout << "hey you can drive now" << endl;
+            break;
+        case 18:
+            cout << "go buy some lotto tickets" << endl;
+            break;
+        case 21:
+            cout << "buy me some beer" << endl;
+            break;
+        default:
+            cout << "sorry you get nothing" << endl;
+    }
 }
