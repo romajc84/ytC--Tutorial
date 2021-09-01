74bd89dd89863deac863cbb1616ca1bf2fd03b98
romajc84 <joeyroman84@gmail.com>
2021-09-01 12:20:48 -0400

    33 - Create an Array Using Loops

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-    int bucky[5] = {66, 75, 2, 43, 99};
+    int bucky[9];
 
-    cout << bucky[2] << endl;
+    cout << "Element  -  Value" << endl;
+
+    for (int x = 0; x < 9; x++) {
+        bucky[x] = 99;
+
+        cout << x << "   -----   " << bucky[x] << endl;
+    }
 }
