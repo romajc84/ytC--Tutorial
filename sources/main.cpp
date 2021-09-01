86e8f75f563b2b20a0e42bceb4e41ce6eca3d76f
romajc84 <joeyroman84@gmail.com>
2021-09-01 16:16:06 -0400

    35 - Passing Arrays to Functions

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-int main() {
-    int tuna[5] = {20, 54, 76, 832, 546};
-    int sum = 0;
+void printArray(int theArray[], int sizeOfArray);
 
-    for (int x = 0; x < 5; x++) {
-        sum += tuna[x];
-        cout << sum << endl;
+int main() {
+    int bucky[3] = {20, 54, 675};
+    int jessica[6] = {54, 24, 7, 8, 9, 99};
+
+    printArray(jessica, 6);
+}
+
+void printArray(int theArray[], int sizeOfArray) {
+    for (int x = 0; x < sizeOfArray; x++) {
+        cout << theArray[x] << endl;
     }
 }
