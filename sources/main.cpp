48de2e1f307e9db65d9dddb61fd7911b41fb654e
romajc84 <joeyroman84@gmail.com>
2021-09-01 16:45:18 -0400

    36 - Multidimensional Arrays

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,17 +2,11 @@
 
 using namespace std;
 
-void printArray(int theArray[], int sizeOfArray);
-
 int main() {
-    int bucky[3] = {20, 54, 675};
-    int jessica[6] = {54, 24, 7, 8, 9, 99};
+    int sally[2][3] = {{2, 3, 4}, {8, 9, 10}};
 
-    printArray(jessica, 6);
-}
+    // 2  3  4
+    // 8  9 10
 
-void printArray(int theArray[], int sizeOfArray) {
-    for (int x = 0; x < sizeOfArray; x++) {
-        cout << theArray[x] << endl;
-    }
+    cout << sally[0][2] << endl;
 }
