df18a392c5615c2302ff5272e605e149fde5c258
romajc84 <joeyroman84@gmail.com>
2021-09-01 08:28:38 -0400

    31 - Recursion

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,18 +2,13 @@
 
 using namespace std;
 
-void printNumber(int x) {
-    cout << "i am printing an integer " << x << endl;
+int factorialFinder(int x) {
+    if (x == 1) {
+        return 1;
+    } else {
+        return x * factorialFinder(x - 1);
+    }
 }
-
-void printNumber(float x) {
-    cout << "now i am printing a float " << x << endl;
-}
-
 int main() {
-    int a = 54;
-    float b = 32.4896;
-
-    printNumber(a);
-    printNumber(b);
+    cout << factorialFinder(5) << endl;
 }
