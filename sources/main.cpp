61bc94543872bde906fecb1ee6ba0447dee9009c
romajc84 <joeyroman84@gmail.com>
2021-09-02 09:52:13 -0400

    39 - Pass by Reference with Pointers

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,12 +2,26 @@
 
 using namespace std;
 
+void passByValue(int x);
+void passByReference(int *x);
+
 int main() {
-    int fish = 5;
-    cout << &fish << endl;
+    int betty = 13;
+    int sandy = 13;
 
-    int *fishPointer;
-    fishPointer = &fish;
+    passByValue(betty);
+    passByReference(&sandy);
 
-    cout << fishPointer << endl;
+    cout << "betty is now " << betty << endl;
+    cout << "sandy is now " << sandy << endl;
+}
+
+void passByValue(int x) {
+    // x is just a copy of betty
+    x = 99;
+}
+
+void passByReference(int *x) {
+    // this x actually changes the memory of sandy
+    *x = 66;
 }
