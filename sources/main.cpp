b1b61243990bee3740242e42d06b1624fe9ad7c4
romajc84 <joeyroman84@gmail.com>
2021-09-02 10:02:40 -0400

    40 - sizeof

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,26 +2,9 @@
 
 using namespace std;
 
-void passByValue(int x);
-void passByReference(int *x);
-
 int main() {
-    int betty = 13;
-    int sandy = 13;
+    double bucky[10];
+    cout << sizeof(bucky) << endl;
 
-    passByValue(betty);
-    passByReference(&sandy);
-
-    cout << "betty is now " << betty << endl;
-    cout << "sandy is now " << sandy << endl;
-}
-
-void passByValue(int x) {
-    // x is just a copy of betty
-    x = 99;
-}
-
-void passByReference(int *x) {
-    // this x actually changes the memory of sandy
-    *x = 66;
+    cout << sizeof(bucky) / sizeof(bucky[0]) << endl;
 }
