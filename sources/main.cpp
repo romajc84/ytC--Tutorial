4dd6a457814602ad133a8dbb5651509da4d167eb
romajc84 <joeyroman84@gmail.com>
2021-09-02 09:43:34 -0400

    38 - Introduction to Pointers

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main() {
-    int bertha[2][3] = {{1, 2, 3}, {7, 8, 9}};
+    int fish = 5;
+    cout << &fish << endl;
 
-    for (int row = 0; row < 2; row++) {
-        for (int column = 0; column < 3; column++) {
-            cout << bertha[row][column] << " ";
-        }
-        cout << endl;
-    }
+    int *fishPointer;
+    fishPointer = &fish;
+
+    cout << fishPointer << endl;
 }
