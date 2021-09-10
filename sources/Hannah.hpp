17e2e9e0264a4226e36ddd3e1763aa7b403d35fa
romajc84 <joeyroman84@gmail.com>
2021-09-10 09:43:32 -0400

    49- this

---
+++ sources/Hannah.hpp
@@ -0,0 +1,14 @@
+#ifndef HANNAH_H
+#define HANNAH_H
+#pragma once
+
+class Hannah {
+
+public:
+  Hannah(int);
+  void printCrap();
+
+private:
+  int h;
+};
+#endif
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,23 +1,11 @@
+#include "Hannah.cpp"
+#include "Hannah.hpp"
 #include <iostream>
 
 using namespace std;
 
-class StankFist {
-   public:
-    StankFist() { stinkyVar = 0; }
-
-   private:
-    int stinkyVar;
-
-    friend void stinkysFriend(StankFist &sfo);
-};
-
-void stinkysFriend(StankFist &sfo) {
-    sfo.stinkyVar = 99;
-    cout << sfo.stinkyVar << endl;
-}
-
 int main() {
-    StankFist bob;
-    stinkysFriend(bob);
+
+  Hannah ho(23);
+  ho.printCrap();
 }
