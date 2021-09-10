13eabd68d98ce4696dc5d402589cdf79d8d643e7
romajc84 <joeyroman84@gmail.com>
2021-09-10 10:00:12 -0400

    50/51 - Operator Overloading

---
+++ sources/Sally.cpp
@@ -0,0 +1,14 @@
+#include "Sally.hpp"
+#include <iostream>
+
+using namespace std;
+
+Sally::Sally() {}
+
+Sally::Sally(int a) { num = a; }
+
+Sally Sally::operator+(Sally aso) {
+  Sally brandNew;
+  brandNew.num = num + aso.num;
+  return (brandNew);
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,11 +1,15 @@
-#include "Hannah.cpp"
-#include "Hannah.hpp"
+#include "Sally.cpp"
+#include "Sally.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
-  Hannah ho(23);
-  ho.printCrap();
+  Sally a(34);
+  Sally b(21);
+  Sally c;
+
+  c = a + b;
+  cout << c.num << endl;
 }
